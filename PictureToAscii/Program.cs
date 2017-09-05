a8d025ff8c3621c931a7ad8777a0245e230467c1
Translive <translivie@gmail.com>
2017-09-06 01:12:04 +0900

    init

---
+++ PictureToAscii/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PictureToAscii
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
